a12b82960d9830c75b19d9da53232e49e29516e6
nbdunks <thenicoleduncan@gmail.com>
2019-09-05 15:00:36 -0700

    made all neccessary changes to my html and javascript to make it functional. i changed the classes in my html to id's

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,8 @@
 // array for the computer's choices
 let computersChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// let alphabet = "qwertyuiopasdfghjklzxcvbnm"
+// let alphabetArr = alphabet.split("")
+// console.log(alphabetArr)
 
 // creating variables that will store the information that the user inputs
 let wins = 0;
@@ -8,20 +11,25 @@ let guessesLeft = 0;
 let yourGuessesSoFar = 0;
 
 // creating variables that hold references to the places in the HTML where i want to display things on the browser
+// let winsText = document.getElementsByClassName("wins-text");
 let winsText = document.getElementById("wins-text");
+console.log(typeof(winsText));
+console.log(typeof(wins));
 let lossesText = document.getElementById("losses-text");
 let guessesLeftText = document.getElementById("guessesLeft-text");
 var yourGuessesSoFarText = document.getElementById("yourGuessesSoFar-text");
+// lossesText.textcontent("anything");
 
     // this function is run whenever the user presses a key
     document.onkeyup = function(event) {
 
         // determines the users guess by pressing any key
         let userGuess = event.key;
-  
+        console.log(userGuess);
+
         // determines what the computers guess will be at random
         let computerGuess = computersChoice[Math.floor(Math.random() * computersChoice.length)];
-  
+        
         // this increments the appropriate numbers
         if (userGuess === computerGuess) {
             wins++;
@@ -33,10 +41,10 @@ var yourGuessesSoFarText = document.getElementById("yourGuessesSoFar-text");
         }
 
           // display the user and computer guesses, and wins/losses/guesses left/your guesses so far
-          winsText.textContent = "wins: " + wins;
-          lossesText.textContent = "losses: " + losses;
-          guessesLeftText.textContent = "guesses left: " + guessesLeft;
-          yourGuessesSoFarText.textContent = "your guesses so far: " + yourGuessesSoFar;
+          winsText.textContent = ("wins: " + wins);
+          lossesText.textContent = ("losses: " + losses);
+          guessesLeftText.textContent = ("guesses left: " + guessesLeft);
+          yourGuessesSoFarText.textContent = ("your guesses so far: " + yourGuessesSoFar);
     };
 
 
