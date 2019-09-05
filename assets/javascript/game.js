5b7dcb5c94b1f676ddb61a246d737d0c0d0d5dfc
nbdunks <thenicoleduncan@gmail.com>
2019-09-04 22:00:15 -0700

    display the user and computer guesses, and wins/losses/guesses left/your guesses so far

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,3 @@
-// <!-- 1. [Watch the demo](https://youtu.be/qTc45Lox97g).
-
-// 2. You're going to make a game just like the one in the video. Essentially, the app randomly picks a letter, 
-// and the user has to guess which letter the app chose. Put the following text on your page:
-
-// 3. Guess what letter I'm thinking of
-
-// 4. Wins: (# of times the user has guessed the letter correctly)
-
-// 5. Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
-
-// 6. Guesses Left: (# of guesses left. This will update)
-
-// 7. Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
-
-// 8. When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
-
-// 9. When the player loses, increase the Losses counter and restart the game without a page refresh 
-// (just like when the user wins). -->
-
 // array for the computer's choices
 let computersChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -49,14 +29,14 @@ var yourGuessesSoFarText = document.getElementById("yourGuessesSoFar-text");
         } else {
             losses++;
             guessesLeft++;
+            yourGuessesSoFar++;
         }
 
-          // Display the user and computer guesses, and wins/losses/ties.
-          userChoiceText.textContent = "You chose: " + userGuess;
-          computerChoiceText.textContent = "The computer chose: " + computerGuess;
+          // display the user and computer guesses, and wins/losses/guesses left/your guesses so far
           winsText.textContent = "wins: " + wins;
           lossesText.textContent = "losses: " + losses;
-          tiesText.textContent = "ties: " + ties;
+          guessesLeftText.textContent = "guesses left: " + guessesLeft;
+          yourGuessesSoFarText.textContent = "your guesses so far: " + yourGuessesSoFar;
     };
 
 
