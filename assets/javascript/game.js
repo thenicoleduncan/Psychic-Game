59ed8043df3a702c334ce72635c7f1e74f410700
nbdunks <thenicoleduncan@gmail.com>
2019-09-04 21:29:21 -0700

    creating variables that hold references to the places in the HTML where we want to display things on the browser. i also added some code to start functioning but its scratch code that i will be tampering with next

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,7 +27,47 @@ let losses = 0;
 let guessesLeft = 0;
 let yourGuessesSoFar = 0;
 
+// creating variables that hold references to the places in the HTML where we want to display things on the browser
+let winsText = document.getElementById("wins-text");
+let lossesText = document.getElementById("losses-text");
+let guessesLeftText = document.getElementById("guessesLeft-text");
+var yourGuessesSoFarText = document.getElementById("yourGuessesSoFar-text");
 
+    // This function is run whenever the user presses a key.
+    document.onkeyup = function(event) {
+
+        // Determines which key was pressed.
+        var userGuess = event.key;
+  
+        // Randomly chooses a choice from the options array. This is the Computer's guess.
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  
+        // Reworked our code from last step to use "else if" instead of lots of if statements.
+  
+        // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+        if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+  
+          if ((userGuess === "r" && computerGuess === "s") ||
+            (userGuess === "s" && computerGuess === "p") || 
+            (userGuess === "p" && computerGuess === "r")) {
+            wins++;
+          } else if (userGuess === computerGuess) {
+            ties++;
+          } else {
+            losses++;
+          }
+  
+          // Hide the directions
+          directionsText.textContent = "";
+  
+          // Display the user and computer guesses, and wins/losses/ties.
+          userChoiceText.textContent = "You chose: " + userGuess;
+          computerChoiceText.textContent = "The computer chose: " + computerGuess;
+          winsText.textContent = "wins: " + wins;
+          lossesText.textContent = "losses: " + losses;
+          tiesText.textContent = "ties: " + ties;
+        }
+      };
 
 
 
